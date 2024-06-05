1aa2a9a401e3fce4170b88fc44e5aa801de49744
helionay <helionaydias@hotmail.com>
2024-06-05 15:54:14 -0300

    inserindo os intens e os valores

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function receberItem() {
   var item = prompt('Digite um item');
-  console.log(item);
-  document.getElementById('p').innerHTML = item;
+
+  var itensValores = {
+    maçã: 2.5,
+    banana: 1.2,
+    laranja: 3.0,
+    abacaxi: 5.5,
+    uva: 4.0,
+  };
+}
+document.getElementById('p').innerHTML = 'item digitado:' + item;
+
+if (itensValores[item.toLowerCase()]) {
+  document.getElementById('valor').innerHTML =
+    'Valor do(a) ' +
+    item +
+    ': R$ ' +
+    itensValores[item.toLowerCase()].toFixed(2);
+} else {
+  document.getElementById('valor').innerHTML = 'Item não encontrado.';
 }
 
 // Chama a função para que ela seja executada
